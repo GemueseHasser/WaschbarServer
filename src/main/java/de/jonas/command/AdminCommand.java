20584f519a6a47d374da764e2991d3086e31c1a6
Jonas Lobe <jonas.lobe.2006@outlook.com>
2022-03-28 16:42:13 +0200

    implement basic admin commands

--- src/main/java/de/jonas/command/AdminCommand.java
+++ src/main/java/de/jonas/command/AdminCommand.java
@@ -4,11 +4,15 @@ import de.jonas.WaschbarServer;
 import de.jonas.object.command.WaschbarCommand;
 import de.jonas.object.unit.WaschbarUser;
 import net.md_5.bungee.api.ChatColor;
+import net.md_5.bungee.api.chat.BaseComponent;
 import net.md_5.bungee.api.chat.ComponentBuilder;
 import net.md_5.bungee.api.chat.TextComponent;
 import org.bukkit.Bukkit;
+import org.bukkit.GameMode;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import org.jetbrains.annotations.Range;
 
 import static net.md_5.bungee.api.chat.ComponentBuilder.FormatRetention.NONE;
 
@@ -22,7 +26,41 @@ import static net.md_5.bungee.api.chat.ComponentBuilder.FormatRetention.NONE;
 @NotNull
 public final class AdminCommand {
 
+    //<editor-fold desc="CONSTANTS">
+    /** Die Nachricht, die einem User gesendet wird, wenn der eingegebene andere User nicht online ist. */
+    @NotNull
+    private static final String PLAYER_NOT_ONLINE = "Der Spieler ist nicht auf diesem Netwerk online!";
+    /** Die Größe des Clears, wenn der Chat gecleared werden soll. */
+    private static final int CHAT_CLEAR_SIZE = 100;
+
+    //<editor-fold desc="command: gamemode">
+    /** Die Nummer für Gamemode Überleben. */
+    private static final int GAMEMODE_SURVIVAL = 0;
+    /** Die Nummer für Gamemode Kreativ. */
+    private static final int GAMEMODE_CREATIVE = 1;
+    /** Die Nummer für Gamemode Abenteuer. */
+    private static final int GAMEMODE_ADVENTURE = 2;
+    /** Die Nummer für Gamemode Zuschauer. */
+    private static final int GAMEMODE_SPECTATOR = 3;
+    //</editor-fold>
+
+    //<editor-fold desc="command: speed">
+    /** Die minimale Geschwindigkeit, auf die man seine Geschwindigkeit setzen kann. */
+    private static final int MIN_SPEED = 1;
+    /** Die maximale Geschwindigkeit, auf die man seine Geschwindigkeit setzen kann. */
+    private static final int MAX_SPEED = 10;
+    /** Die standard Geschwindigkeit beim Laufen. */
+    private static final float DEFAULT_WALK_SPEED = 0.2F;
+    /** Die standard Geschwindigkeit beim Fliegen. */
+    private static final float DEFAULT_FLY_SPEED = 0.1F;
+    /** Der Divisor, durch den die Geschwindigkeit beim Setzen geteilt wird. */
+    private static final int DIVISOR = 10;
+    //</editor-fold>
+
+    //</editor-fold>
+
     //<editor-fold desc="command: nick">
+
     /**
      * Mit diesem Befehl kann sich ein Spieler einen Nickname geben oder kann einem anderen Spieler einen Nickname
      * geben.
@@ -57,7 +95,7 @@ public final class AdminCommand {
             player.sendMessage(
                 new ComponentBuilder(WaschbarServer.getPrefix())
                     .append(
-                        " Der Spieler ist nicht online!",
+                        " " + PLAYER_NOT_ONLINE,
                         NONE
                     ).color(ChatColor.GRAY)
                     .create()
@@ -107,7 +145,7 @@ public final class AdminCommand {
             player.sendMessage(
                 new ComponentBuilder(WaschbarServer.getPrefix())
                     .append(
-                        " Der Spieler ist nicht online!",
+                        " " + PLAYER_NOT_ONLINE,
                         NONE
                     ).color(ChatColor.GRAY)
                     .create()
@@ -153,4 +191,184 @@ public final class AdminCommand {
     }
     //</editor-fold>
 
+    //<editor-fold desc="command: gamemode">
+
+    /**
+     * Es wird der Gamemode-Befehl implementiert, womit man den Gamemode von sich selbst oder von jemandem anderen
+     * editieren kann.
+     *
+     * @param player Der Spieler, der diesen Befehl ausführt.
+     * @param args   Die Argumente, die der Spieler zusätzlich zu dem Befehl eingibt.
+     */
+    @WaschbarCommand(
+        command = "gamemode",
+        minLength = 1,
+        maxLength = 2,
+        permission = "waschbar.gamemode",
+        usage = "/gamemode <gamemode> [<player>]",
+        aliases = {"gm"}
+    )
+    public void gamemode(
+        @NotNull final Player player,
+        @NotNull final String[] args
+    ) {
+        final boolean isSame = args.length == 1;
+        final Player target = isSame ? player : Bukkit.getPlayer(args[1]);
+
+        if (target == null || !target.isOnline()) {
+            player.sendMessage(TextComponent.fromLegacyText(
+                WaschbarServer.getPrefix() + " " + PLAYER_NOT_ONLINE
+            ));
+            return;
+        }
+
+        switch (args[0]) {
+            case "survival":
+            case "überleben":
+            case GAMEMODE_SURVIVAL + "":
+                target.setGameMode(GameMode.SURVIVAL);
+                player.sendMessage(TextComponent.fromLegacyText(
+                    WaschbarServer.getPrefix() + getGamemodeMessage(GAMEMODE_SURVIVAL, isSame, target.getName())
+                ));
+                break;
+
+            case "creative":
+            case "kreativ":
+            case GAMEMODE_CREATIVE + "":
+                target.setGameMode(GameMode.CREATIVE);
+                player.sendMessage(TextComponent.fromLegacyText(
+                    WaschbarServer.getPrefix() + getGamemodeMessage(GAMEMODE_CREATIVE, isSame, target.getName())
+                ));
+                break;
+
+            case "adventure":
+            case "abenteuer":
+            case GAMEMODE_ADVENTURE + "":
+                target.setGameMode(GameMode.ADVENTURE);
+                player.sendMessage(TextComponent.fromLegacyText(
+                    WaschbarServer.getPrefix() + getGamemodeMessage(GAMEMODE_ADVENTURE, isSame, target.getName())
+                ));
+                break;
+
+            case "spectator":
+            case "zuschauer":
+            case GAMEMODE_SPECTATOR + "":
+                target.setGameMode(GameMode.SPECTATOR);
+                player.sendMessage(TextComponent.fromLegacyText(
+                    WaschbarServer.getPrefix() + getGamemodeMessage(GAMEMODE_SPECTATOR, isSame, target.getName())
+                ));
+                break;
+
+            default:
+                player.sendMessage(TextComponent.fromLegacyText(
+                    WaschbarServer.getPrefix() + "Bitte benutze /gamemode <1|2|3>"
+                ));
+                break;
+        }
+    }
+
+    /**
+     * Gibt die Nachricht zurück, die ausgegeben wird, wenn der Gamemode-Befehl erfolgreich ausgeführt wurde.
+     *
+     * @param gm   Der Gamemode, welcher gewählt wurde.
+     * @param same Der Zustand, ob der Spieler selbst den Gamemode gewechselt hat, oder ob der Spieler den Gamemode
+     *             eines anderen Spielers gewechselt hat.
+     * @param name Der Name des Spielers, dessen Gamemode gewechselt wurde.
+     *
+     * @return Die Nachricht, die ausgegeben wird, wenn der Befehl erfolgreich ausgeführt wurde.
+     */
+    private String getGamemodeMessage(
+        @Range(from = 0, to = Integer.MAX_VALUE) final int gm,
+        final boolean same,
+        @Nullable final String name
+    ) {
+        final String gamemode = gm == 0 ? "Überleben" : gm == 1 ? "Kreativ" : gm == 2 ? "Abenteuer" : "Zuschauer";
+        return (same ? "Du wurdest " : "Der Spieler " + name + " wurde ")
+            + "in Gamemode " + ChatColor.RED + gamemode + ChatColor.GRAY + " versetzt!";
+    }
+    //</editor-fold>
+
+    //<editor-fold desc="command: speed">
+
+    /**
+     * Es wird der Befehl implementiert, womit man seine Geschwindigkeit editieren kann.
+     *
+     * @param player Der Spieler, der diesen Befehl ausführt.
+     * @param args   Die Argumente, die der Spieler zusätzlich zu dem Befehl eingibt.
+     */
+    @WaschbarCommand(
+        command = "speed",
+        minLength = 1,
+        maxLength = 1,
+        permission = "waschbar.speed",
+        usage = "/speed <1-10>"
+    )
+    public void speed(
+        @NotNull final Player player,
+        @NotNull final String[] args
+    ) {
+        if (args[0].equalsIgnoreCase("default") || args[0].equalsIgnoreCase("d")) {
+            player.setWalkSpeed(DEFAULT_WALK_SPEED);
+            player.setFlySpeed(DEFAULT_FLY_SPEED);
+
+            player.sendMessage(TextComponent.fromLegacyText(
+                WaschbarServer.getPrefix() + "Deine Geschwindigkeit wurde wieder normalisiert!"
+            ));
+            return;
+        }
+        // declare speed
+        final int speed;
+
+        // try to set speed from arguments
+        try {
+            speed = Integer.parseInt(args[0]);
+        } catch (@NotNull final IllegalArgumentException ignored) {
+            player.sendMessage(TextComponent.fromLegacyText(
+                WaschbarServer.getPrefix() + "Bitte wähle eine Geschwindigkeit zwischen 1 und 10!"
+            ));
+            return;
+        }
+
+        // check if speed is correct
+        if (speed < MIN_SPEED || speed > MAX_SPEED) {
+            player.sendMessage(TextComponent.fromLegacyText(
+                WaschbarServer.getPrefix() + "Bitte wähle eine Geschwindigkeit zwischen 1 und 10!"
+            ));
+            return;
+        }
+
+        // set players speed
+        player.setFlySpeed((float) speed / DIVISOR);
+        player.setWalkSpeed((float) speed / DIVISOR);
+
+        player.sendMessage(TextComponent.fromLegacyText(
+            WaschbarServer.getPrefix() + "Deine Geschwindigkeit wurde auf " + speed + " geändert!"
+        ));
+    }
+    //</editor-fold>
+
+    //<editor-fold desc="command: chatclear">
+    /**
+     * Es wird der Befehl implementiert, womit man seine Geschwindigkeit editieren kann.
+     *
+     * @param player Der Spieler, der diesen Befehl ausführt.
+     * @param args   Die Argumente, die der Spieler zusätzlich zu dem Befehl eingibt.
+     */
+    @WaschbarCommand(
+        command = "chatclear",
+        permission = "waschbar.cc",
+        usage = "/chatclear | /cc",
+        aliases = {"cc"}
+    )
+    public void clearChat(
+        @NotNull final Player player,
+        @NotNull final String[] args
+    ) {
+        for (int i = 0; i < CHAT_CLEAR_SIZE; i++) {
+            for (@NotNull final Player onlinePlayer : Bukkit.getOnlinePlayers()) {
+                onlinePlayer.sendMessage(TextComponent.fromLegacyText(""));
+            }
+        }
+    }
+    //</editor-fold>
 }
